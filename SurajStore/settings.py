6c3f9564d0a92575864fe13a0c472259e0493821
Sumit Raj <rajsumitpandya6@gmail.com>
2025-09-07 14:30:28 +0530

    Added whitenoise and static files setup

--- SurajStore/settings.py
+++ SurajStore/settings.py
@@ -112,6 +112,7 @@ SITE_ID = 1
 # ------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ------------------------
 # URLs and templates
